07051c34835f92df9a5a1ca6d6ad0e6f3ba568e4
YaoBing Xiao <xiaoyaobing@uniontech.com>
2026-08-27 20:59:15 +0800

    curve/wlf_curve: add destruction listener support

--- curve/wlf_curve.c
+++ curve/wlf_curve.c
@@ -9,9 +9,11 @@ void wlf_curve_init(struct wlf_curve *curve,
 	assert(impl->value_at);
 	assert(impl->destroy);
 
-	curve->impl = impl;
-
-	wlf_signal_init(&curve->events.destroy);
+	*curve = (struct wlf_curve) {
+		.impl = impl,
+		.listener = NULL,
+		.user_data = NULL,
+	};
 }
 
 float wlf_curve_value_at(const struct wlf_curve *curve, float t) {
@@ -23,13 +25,19 @@ void wlf_curve_destroy(struct wlf_curve *curve) {
 		return;
 	}
 
-	wlf_signal_emit_mutable(&curve->events.destroy, curve);
-
-	assert(wlf_linked_list_empty(&curve->events.destroy.listener_list));
+	if (curve->listener != NULL && curve->listener->destroy != NULL) {
+		curve->listener->destroy(curve->user_data, curve);
+	}
 
 	if (curve->impl && curve->impl->destroy) {
 		curve->impl->destroy(curve);
 	} else {
 		free(curve);
 	}
+}
+
+void wlf_curve_add_listener(struct wlf_curve *curve,
+		const struct wlf_curve_listener *listener, void *data) {
+	curve->listener = listener;
+	curve->user_data = data;
 }
