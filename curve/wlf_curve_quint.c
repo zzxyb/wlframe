b391fc9e3e0a055344dfe6264b0c7360ab111302
YaoBing Xiao <xiaoyaobing@uniontech.com>
2026-08-29 00:22:12 +0800

    curve/quint: return concrete out-in-quint curve

--- curve/wlf_curve_quint.c
+++ curve/wlf_curve_quint.c
@@ -100,7 +100,7 @@ struct wlf_curve_quint *wlf_curve_in_out_quint_create(void) {
 	return curve;
 }
 
-struct wlf_curve *wlf_curve_out_in_quint_create(void) {
+struct wlf_curve_quint *wlf_curve_out_in_quint_create(void) {
 	struct wlf_curve_quint *curve = malloc(sizeof(*curve));
 	if (curve == NULL) {
 		wlf_log_errno(WLF_ERROR, "failed to allocate wlf_curve_quint");
@@ -110,7 +110,7 @@ struct wlf_curve *wlf_curve_out_in_quint_create(void) {
 	wlf_curve_init(&curve->base, &out_in_quint_impl);
 	curve->type = WLF_CURVE_OUT_IN;
 
-	return &curve->base;
+	return curve;
 }
 
 bool wlf_curve_is_quint(const struct wlf_curve *curve) {
