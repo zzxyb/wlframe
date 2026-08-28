7bc1d5e2b3469c8eb64d548d235f18087cd52356
YaoBing Xiao <xiaoyaobing@uniontech.com>
2026-08-29 00:21:25 +0800

    curve/cubic: return concrete in-out-cubic curve

--- include/wlf/curve/wlf_curve_cubic.h
+++ include/wlf/curve/wlf_curve_cubic.h
@@ -60,7 +60,7 @@ struct wlf_curve_cubic *wlf_curve_out_cubic_create(void);
  *
  * @return Pointer to the created curve, or NULL on allocation failure.
  */
-struct wlf_curve *wlf_curve_in_out_cubic_create(void);
+struct wlf_curve_cubic *wlf_curve_in_out_cubic_create(void);
 
 /**
  * @brief Creates an ease-out-in cubic curve.
