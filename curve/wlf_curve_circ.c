1e56e5a7aca7aa7f21adf93b378b48f997f46105
YaoBing Xiao <xiaoyaobing@uniontech.com>
2026-08-29 00:21:22 +0800

    curve/circ: return concrete out-circ curve

--- curve/wlf_curve_circ.c
+++ curve/wlf_curve_circ.c
@@ -74,7 +74,7 @@ struct wlf_curve_circ *wlf_curve_in_circ_create(void) {
 	return curve;
 }
 
-struct wlf_curve *wlf_curve_out_circ_create(void) {
+struct wlf_curve_circ *wlf_curve_out_circ_create(void) {
 	struct wlf_curve_circ *curve = malloc(sizeof(*curve));
 	if (curve == NULL) {
 		wlf_log_errno(WLF_ERROR, "failed to allocate wlf_curve_circ");
@@ -84,7 +84,7 @@ struct wlf_curve *wlf_curve_out_circ_create(void) {
 	wlf_curve_init(&curve->base, &out_circ_impl);
 	curve->type = WLF_CURVE_OUT;
 
-	return &curve->base;
+	return curve;
 }
 
 struct wlf_curve *wlf_curve_in_out_circ_create(void) {
