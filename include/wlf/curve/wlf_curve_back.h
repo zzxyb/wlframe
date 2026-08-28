a484706fbd08e6014634844bc1e8c6d634076972
YaoBing Xiao <xiaoyaobing@uniontech.com>
2026-08-29 00:20:31 +0800

    curve/back: return concrete out-in-back curve

--- include/wlf/curve/wlf_curve_back.h
+++ include/wlf/curve/wlf_curve_back.h
@@ -79,7 +79,7 @@ struct wlf_curve_back *wlf_curve_in_out_back_create(float overshoot);
  *                  Typical value: 1.70158 for standard back easing.
  * @return Pointer to the created curve, or NULL on allocation failure.
  */
-struct wlf_curve *wlf_curve_out_in_back_create(float overshoot);
+struct wlf_curve_back *wlf_curve_out_in_back_create(float overshoot);
 
 /**
  * @brief Checks if a curve is a back curve.
