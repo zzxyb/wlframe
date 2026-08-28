7cbe1f14a54f2268bdb0dfc5f459b3ebeb628923
YaoBing Xiao <xiaoyaobing@uniontech.com>
2026-08-29 00:21:20 +0800

    curve/bounce: return concrete out-bounce curve

--- curve/wlf_curve_bounce.c
+++ curve/wlf_curve_bounce.c
@@ -74,7 +74,7 @@ struct wlf_curve_bounce *wlf_curve_in_bounce_create(void) {
 	return curve;
 }
 
-struct wlf_curve *wlf_curve_out_bounce_create(void) {
+struct wlf_curve_bounce *wlf_curve_out_bounce_create(void) {
 	struct wlf_curve_bounce *curve = malloc(sizeof(*curve));
 	if (curve == NULL) {
 		wlf_log_errno(WLF_ERROR, "failed to allocate wlf_curve_bounce");
@@ -84,7 +84,7 @@ struct wlf_curve *wlf_curve_out_bounce_create(void) {
 	wlf_curve_init(&curve->base, &out_bounce_impl);
 	curve->type = WLF_CURVE_OUT;
 
-	return &curve->base;
+	return curve;
 }
 
 struct wlf_curve *wlf_curve_in_out_bounce_create(void) {
