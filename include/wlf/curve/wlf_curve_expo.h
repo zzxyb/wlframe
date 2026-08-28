810528a7a4dcc40a753e55bccc2aad5db2f518c6
YaoBing Xiao <xiaoyaobing@uniontech.com>
2026-08-29 00:21:31 +0800

    curve/expo: return concrete out-in-expo curve

--- include/wlf/curve/wlf_curve_expo.h
+++ include/wlf/curve/wlf_curve_expo.h
@@ -72,7 +72,7 @@ struct wlf_curve_expo *wlf_curve_in_out_expo_create(void);
  *
  * @return Pointer to the created curve, or NULL on allocation failure.
  */
-struct wlf_curve *wlf_curve_out_in_expo_create(void);
+struct wlf_curve_expo *wlf_curve_out_in_expo_create(void);
 
 /**
  * @brief Checks if a curve is an exponential curve.
