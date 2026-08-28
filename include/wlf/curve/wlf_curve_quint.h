f0d99da83e82bb828891dd92fde50f054e18b90d
YaoBing Xiao <xiaoyaobing@uniontech.com>
2026-08-29 00:22:11 +0800

    curve/quint: return concrete in-out-quint curve

--- include/wlf/curve/wlf_curve_quint.h
+++ include/wlf/curve/wlf_curve_quint.h
@@ -60,7 +60,7 @@ struct wlf_curve_quint *wlf_curve_out_quint_create(void);
  *
  * @return Pointer to the created curve, or NULL on allocation failure.
  */
-struct wlf_curve *wlf_curve_in_out_quint_create(void);
+struct wlf_curve_quint *wlf_curve_in_out_quint_create(void);
 
 /**
  * @brief Creates an ease-out-in quintic curve.
